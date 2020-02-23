7887c4ece6edb10fe2c4dad3d682ff9bdde13522
Ivan Saldano <ivanms1@gmail.com>
2020-02-22 22:13:04 -0300

    Updates

---
+++ precache-manifest.ce38617285a2154fc529e4223fd15761.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572ab60a213a7fdcf5587a08d6ea2037",
+    "url": "/vidayfamilia/index.html"
+  },
+  {
+    "revision": "8c95838b4eba49a2f41a",
+    "url": "/vidayfamilia/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "a9a20b8591155f0f3846",
+    "url": "/vidayfamilia/static/css/main.59cb0ce5.chunk.css"
+  },
+  {
+    "revision": "8c95838b4eba49a2f41a",
+    "url": "/vidayfamilia/static/js/2.ecd76294.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/vidayfamilia/static/js/2.ecd76294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a20b8591155f0f3846",
+    "url": "/vidayfamilia/static/js/main.0b0bb50d.chunk.js"
+  },
+  {
+    "revision": "bc0aaf9cfefad75f447e",
+    "url": "/vidayfamilia/static/js/runtime-main.ebb28542.js"
+  },
+  {
+    "revision": "6110639c46d84dee1e6c00616de1bdc8",
+    "url": "/vidayfamilia/static/media/home-bg.6110639c.jpg"
+  },
+  {
+    "revision": "5d586fa7620b25b40362aeca17ef7d3b",
+    "url": "/vidayfamilia/static/media/logo.5d586fa7.jpeg"
+  }
+]);
